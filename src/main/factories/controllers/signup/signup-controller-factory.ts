d0b26846506d533c611baf75f086cabf51000eee
Iago Silva Lima <iagocrouchlima@gmail.com>
2021-08-20 23:16:41 -0300

    refactor: separate factories in folders

---
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
+import { DbAddAccount } from '../../../../data/usecases/add-acount/db-add-acount'
+import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+import { Controller } from '../../../../presentation/protocols'
+import { makeSignUpValidation } from './signup-validation-factory'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+
+export const makeSignUpController = (): Controller => {
+  const bcryptAdapter = new BcryptAdapter(12)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), makeDbAuthentication())
+  return makeLogControllerDecorator(signUpController)
+}
