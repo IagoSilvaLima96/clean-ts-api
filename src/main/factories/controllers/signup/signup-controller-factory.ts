38ad18b9a218657fc43a0215fab86343060b5f52
Iago Silva Lima <iagocrouchlima@gmail.com>
2021-08-21 22:58:33 -0300

    refactor: move dbAddAccount useCase factory to a folder

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,16 +1,11 @@
 import { SignUpController } from '../../../../presentation/controllers/login/signup/signup-controller'
-import { DbAddAccount } from '../../../../data/usecases/add-acount/db-add-acount'
-import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
 import { Controller } from '../../../../presentation/protocols'
 import { makeSignUpValidation } from './signup-validation-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
 
 export const makeSignUpController = (): Controller => {
-  const bcryptAdapter = new BcryptAdapter(12)
-  const accountMongoRepository = new AccountMongoRepository()
-  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), makeDbAuthentication())
+  const signUpController = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
   return makeLogControllerDecorator(signUpController)
 }
