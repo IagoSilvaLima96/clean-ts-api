bd3452285682f743690ed0c72d8831722d546373
Iago Silva Lima <iagocrouchlima@gmail.com>
2021-08-23 06:59:56 -0300

    refactor: organize main layer folders

---
+++ src/main/factories/usecases/account/add-account/db-add-account-factory.ts
@@ -0,0 +1,10 @@
+import { DbAddAccount } from '../../../../../data/usecases/add-acount/db-add-acount'
+import { BcryptAdapter } from '../../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+import { AddAccount } from '../../../../../domain/usecases/add-account'
+
+export const makeDbAddAccount = (): AddAccount => {
+  const bcryptAdapter = new BcryptAdapter(12)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
+}
