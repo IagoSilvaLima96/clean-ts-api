fcace6473258ef09a48e00acd26f38daf4b6c5a1
Iago Silva Lima <iagocrouchlima@gmail.com>
2021-08-23 21:11:16 -0300

    feat: ensure DbLoadSurveysUseCase returns Surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.ts
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -8,7 +8,6 @@ export class DbLoadSurveys implements LoadSurveys {
   }
 
   async load (): Promise<SurveyModel[]> {
-    await this.loadSurveysRepository.loadAll()
-    return []
+    return await this.loadSurveysRepository.loadAll()
   }
 }
