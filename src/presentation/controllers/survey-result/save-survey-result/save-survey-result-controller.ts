6dfb5e0a05069f9b40ba92cd873efd342a56c53c
Iago Silva Lima <iagocrouchlima@gmail.com>
2021-08-25 14:29:13 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResultUseCase with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,12 +1,24 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById, SurveyAnswerModel } from './save-survey-result-controller-protocols'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  LoadSurveyById,
+  SurveyAnswerModel,
+  SaveSurveyResult,
+  SaveSurveyResultModel
+} from './save-survey-result-controller-protocols'
 import { ok, badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) { }
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const { accountId } = httpRequest
       const { surveyId } = httpRequest.params
       const { answer } = httpRequest.body
       const survey = await this.loadSurveyById.loadById(surveyId)
@@ -18,7 +30,13 @@ export class SaveSurveyResultController implements Controller {
       if (!this.answerExists(survey.answers, answer)) {
         return badRequest(new InvalidParamError('answer'))
       }
-
+      const saveSurveyResultModel: SaveSurveyResultModel = {
+        surveyId,
+        accountId: accountId as string,
+        answer,
+        date: new Date()
+      }
+      await this.saveSurveyResult.save(saveSurveyResultModel)
       return ok({})
     } catch (error) {
       return serverError(error)
