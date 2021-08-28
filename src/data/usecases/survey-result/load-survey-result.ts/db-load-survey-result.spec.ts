67f5bb64c20167fde966a1185e5580bca5aee614
Iago Silva Lima <iagocrouchlima@gmail.com>
2021-08-28 00:53:06 -0300

    test: ensure DbLoadSurveyResultUseCase throws if LoadSurveyResultRepository throws

--- src/data/usecases/survey-result/load-survey-result.ts/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result.ts/db-load-survey-result.spec.ts
@@ -3,6 +3,7 @@ import {
   LoadSurveyResultRepository,
   mockLoadSurveyResultRepository
 } from './db-load-survey-result-protocols'
+import { throwError } from '@/domain/test'
 
 type SutTypes = {
   sut: DbLoadSurveyResult
@@ -25,5 +26,12 @@ describe('DbLoadSurveyResult UseCase', () => {
     const surveyId = 'any_survey_id'
     await sut.load(surveyId)
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith(surveyId)
+  })
+
+  test('Should throw error if LoadSurveyResultRepository throws error', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
+    const promise = sut.load('any_survey_id')
+    await expect(promise).rejects.toThrow()
   })
 })
