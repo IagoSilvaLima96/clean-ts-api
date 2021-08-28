2d6513fff87ff48d830e9d9dbab7087a42e26b15
Iago Silva Lima <iagocrouchlima@gmail.com>
2021-08-28 00:39:22 -0300

    feat: ensure DbLoadSurveyResultUseCase calls LoadSurveyResultRepository with correct surveyId

---
+++ src/data/usecases/survey-result/load-survey-result.ts/db-load-survey-result.ts
@@ -0,0 +1,14 @@
+import {
+  LoadSurveyResult,
+  SurveyResultModel,
+  LoadSurveyResultRepository
+} from './db-load-survey-result-protocols'
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+
+  async load (surveyId: string): Promise<SurveyResultModel | null> {
+    await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    return null
+  }
+}
