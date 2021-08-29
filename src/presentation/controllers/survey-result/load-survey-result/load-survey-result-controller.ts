393a7bb0166beb50714cdffa2a0c33d60b0c3609
Iago Silva Lima <iagocrouchlima@gmail.com>
2021-08-29 11:36:22 -0300

    feat: ensure LoadSurveyResultController returns 400 if LoadSurveyById returns null

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -4,13 +4,17 @@ import {
   HttpResponse,
   LoadSurveyById
 } from './load-survey-result-controller-protocols'
-import { ok } from '@/presentation/helpers/http/http-helper'
+import { ok, badRequest } from '@/presentation/helpers/http/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { surveyId } = httpRequest.params
-    await this.loadSurveyById.loadById(surveyId)
+    const survey = await this.loadSurveyById.loadById(surveyId)
+    if (!survey) {
+      return badRequest(new InvalidParamError('surveyId'))
+    }
     return ok({})
   }
 }
